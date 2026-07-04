d64bc316a7c9ec6caae30a565e11a37099e1a7ba
Luca Forzisi <82125008+zodaz0@users.noreply.github.com>
2026-07-05 00:17:49 +1000

    Updated ServerScriptService/Components/Kill.lua

---
+++ ServerScriptService/Components/Kill.lua
@@ -0,0 +1,31 @@
+-- @ScriptType: ModuleScript
+local Kill = {}
+Kill.__index = Kill
+
+function Kill.new(tycoon, instance)
+	local self = setmetatable({}, Kill)
+	self.Tycoon = tycoon
+	self.Instance = instance
+	return self
+end
+
+function Kill:Init()
+	self.Instance.Touched:Connect(function(...)
+		self:OnTouched(...)
+	end)
+end
+
+
+function Kill:OnTouched(hit)
+	local owner = self.Tycoon.Owner
+	local character = hit:FindFirstAncestorWhichIsA("Model")
+	local humanoid = character and character:FindFirstChild("Humanoid")
+	
+	
+	if humanoid and character ~= owner.Character then
+		humanoid.Health = 0
+	end
+end
+
+
+return Kill
